26415445e6f55c44b3534b42d575bd90d9445ca4
delokoseni <artur.b3lsh@gmail.com>
2022-09-11 14:37:17 +0700

    Добавлена функция полного вывода класса employee

--- lab2/atd.h
+++ lab2/atd.h
@@ -11,6 +11,9 @@ public:
 	experience(float w, int a, float m);
 	experience(int x);
 	experience();
+	float getworkingyears();
+	int getarmy();
+	float getmaternityleave();
 private:
 	float workingyears; //кол-во реально отработанных лет
 	int army; //кол-во лет в армии, если сохранялось рабочее место
@@ -38,12 +41,28 @@ experience::experience() {
 	maternityleave = 0;
 }
 
+//методы получения данных из класса experience
+float experience::getworkingyears() {
+	return this->workingyears;
+}
+
+int experience::getarmy() {
+	return this->army;
+}
+
+float experience::getmaternityleave() {
+	return this->maternityleave;
+}
+
 //АТД отработанные за месяц часы
 class hours {
 public:
 	hours(int n, int o, int w);
 	hours(int x);
 	hours();
+	int getnormal();
+	int getovertime();
+	int getweekends();
 private:
 	int normal; //кол-во часов, отработанных по графику
 	int overtime; //кол-во часов, отработанных сверхурочно
@@ -71,33 +90,55 @@ hours::hours() {
 	weekends = 0;
 }
 
+//методы получения данных из класса hours
+int hours::getnormal() {
+	return this->normal;
+}
+int hours::getovertime() {
+	return this->overtime;
+}
+int hours::getweekends() {
+	return this->weekends;
+}
+
 //АТД подчиненные
 class subordinates {
 public:
 	subordinates(int a, float A);
 	subordinates(int x);
 	subordinates();
+	int getamount();
+	float getasos();
 private:
 	int amount; //кол-во подчиненных
-	float ASoS; //average seniority of subordinates - средний стаж подчиненных
+	float asos; //average seniority of subordinates - средний стаж подчиненных
 };
 
 //конструктор класса subordinates со всеми параметрами
 subordinates::subordinates(int a, float A) {
 	amount = a;
-	ASoS = A;
+	asos = A;
 }
 
 //конструктор класса subordinates с одним параметром
 subordinates::subordinates(int x) {
 	amount = x;
-	ASoS = x;
+	asos = x;
 }
 
 //конструктор класса subordinates без параметров
 subordinates::subordinates() {
 	amount = 0;
-	ASoS = 0;
+	asos = 0;
+}
+
+//методы получения данных из класса subordinates
+int subordinates::getamount() {
+	return this->amount;
+}
+
+float subordinates::getasos() {
+	return this->asos;
 }
 
 //АТД должность
@@ -106,6 +147,9 @@ public:
 	jobtitle(string j, int h, subordinates s);
 	jobtitle(int x);
 	jobtitle();
+	string getjtitle();
+	int gethourlycost();
+	subordinates getsubs();
 private:
 	string jtitle; //наименование должности
 	int hourlycost; //стоимость часа работы
@@ -123,7 +167,7 @@ jobtitle::jobtitle(string j, int h, subordinates s) {
 jobtitle::jobtitle(int x) {
 	jtitle = to_string(x);
 	hourlycost = x;
-	subordinates subs(x, x);
+	subordinates subs(x);
 }
 
 //конструктор класса jobtitle без параметров
@@ -133,12 +177,27 @@ jobtitle::jobtitle() {
 	subordinates subs;
 }
 
+//методы получения данных из класса jobtitle
+string jobtitle::getjtitle() {
+	return this->jtitle;
+}
+
+int jobtitle::gethourlycost() {
+	return this->hourlycost;
+}
+
+subordinates jobtitle::getsubs() {
+	return this->subs;
+}
+
+
 //АТД Сотрудник
 class employee {
 public:
 	employee(int i, experience e, hours h, jobtitle j);
 	employee(int x);
 	employee();
+	void output();
 private:
 	int id; //индивидуальный номер
 	experience exp; //стаж
@@ -154,7 +213,7 @@ employee::employee(int i, experience e, hours h, jobtitle j) {
 	jt = j;
 }
 
-//конструктор класса employee со всеми параметрами
+//конструктор класса employee с одним параметром
 employee::employee(int x) {
 	id = x;
 	experience exp(x);
@@ -168,4 +227,20 @@ employee::employee() {
 	experience exp;
 	hours hour;
 	jobtitle jt;
+}
+
+//метод полного вывода
+void employee::output() {
+	subordinates s = this->jt.getsubs();
+	cout << "ID: " << this->id << endl;
+	cout << "Стаж (отработано лет): " << this->exp.getworkingyears() << endl;
+	cout << "Стаж (кол-во лет в армии): " << this->exp.getarmy() << endl;
+	cout << "Стаж (кол-во лет в декретном отпуске): " << this->exp.getmaternityleave() << endl;
+	cout << "Отработано часов за месяц(по графику): " << this->hour.getnormal() << endl;
+	cout << "Отработано часов за месяц(сверхурочно): " << this->hour.getovertime() << endl;
+	cout << "Отработано часов за месяц(в выходные дни): " << this->hour.getweekends() << endl;
+	cout << "Должность: " << this->jt.getjtitle() << endl;
+	cout << "Стоимость часа работы: " << this->jt.gethourlycost() << endl;
+	cout << "Кол-во подчиненных: " << s.getamount() << endl;
+	cout << "Средний стаж подчиненных: " << s.getasos() << endl;
 }
--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -6,14 +6,19 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    employee human1();
+    employee human1;
     employee human2(1);
     subordinates people(1, 1);
     char c;
     string str = "Менеджер";
     jobtitle man(str, 140, people);
+    experience exp(1);
+    hours hour(1);
+    jobtitle jt(1);
+    employee human3(1, exp, hour, jt);
     do{
-        
+        human2.output();
+        human3.output();
         printf("Для повтора программы нажмите любую клавишу.\n");
         printf("Для выхода из программы нажмите ESC.\n");
         c = _getch();
